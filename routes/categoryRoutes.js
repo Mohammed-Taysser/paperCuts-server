8e2254630ec523c7cc7fd57f5c8d3626190ce34d
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-10 17:12:15 +0200

    add all & view category routes

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const controller = require('../controllers/categoryController');
+
+const router = express.Router();
+
+router.get('/', controller.all);
+router.get('/:slug', controller.view);
+
+module.exports = router;
