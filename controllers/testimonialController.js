66a4d8e73e1b0149a25fcff00a81d23656d65856
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-10 16:15:32 +0200

    create `get all testimonials` controller

---
+++ controllers/testimonialController.js
@@ -0,0 +1,14 @@
+const schema = require('../models/testimonialSchema');
+const statusCode = require('../utilities/statusCode');
+const catchError = require('../utilities/catchError');
+
+exports.all = async (_, response) => {
+  await schema
+    .find()
+    .then((results) => {
+      response.json(results);
+    })
+    .catch((error) =>
+      catchError(response, statusCode.error.serverError, error.message)
+    );
+};
