647bad9e9f97234ff6bfdcde8e36199a4fbc3e74
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-10 17:42:54 +0200

    create `all & view` controllers

---
+++ controllers/eventController.js
@@ -0,0 +1,30 @@
+const schema = require('../models/eventSchema');
+const statusCode = require('../utilities/statusCode');
+const catchError = require('../utilities/catchError');
+
+exports.all = async (_, response) => {
+  await schema
+    .find()
+    .then((results) => {
+      response.json(results);
+    })
+    .catch((error) =>
+      catchError(response, statusCode.error.serverError, error.message)
+    );
+};
+
+exports.view = async (request, response) => {
+  const slug = request.params.slug;
+  await schema
+    .find({ slug })
+    .then((results) => {
+      if (results.length === 1) {
+        response.status(statusCode.success.ok).json(results[0]);
+      } else {
+        catchError(response, statusCode.error.notFound, 'event not found');
+      }
+    })
+    .catch((error) =>
+      catchError(response, statusCode.error.notFound, error.message)
+    );
+};
