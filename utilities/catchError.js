f326910bc003311c32d903ff368bc0b248f91f5d
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-22 22:13:03 +0200

    add catchError doc

--- utilities/catchError.js
+++ utilities/catchError.js
@@ -1,8 +1,10 @@
 /**
  * return response with status code and message as json
- * @param {*} response the response object
- * @param {Number} code response status code
- * @param {String} message the error message
+ * @example
+ *  catchError(response, 500, 'Error Occur')
+ * @param {*} response Express HTTP Response
+ * @param {Number} code rStatus code to send on fail
+ * @param {String|Object} message the error message
  * @returns create response with status and json error message
  */
 function catchError(response, code = 404, message = 'Error Went Occur') {
