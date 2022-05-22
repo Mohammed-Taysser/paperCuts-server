50e7ad6acdc7319b0e4ee7713966ea5e46fd025e
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-22 22:18:43 +0200

    update schema files name

---
+++ models/books.schema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const booksSchema = new Schema({
+  title: String,
+  slug: String,
+  image: String,
+  info: String,
+  extraInfo: String,
+  publisher: String,
+  publishAr: Date,
+  price: Number,
+  stars: Number,
+  pages: Number,
+  edition: Number,
+  pdfSize: Number,
+  reviews: Number,
+  language: [String],
+  types: [{ type: String, default: "paper" }],
+  category: [
+    {
+      title: String,
+      slug: String,
+    },
+  ],
+  author: {
+    type: Object,
+    avatar: String,
+    username: String,
+  },
+});
+
+module.exports = mongoose.model("Books", booksSchema);
