bf57e8ad0d49067a9df0903c22be115b941132b8
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-22 21:47:51 +0200

    update errors message

--- validations/register.validate.js
+++ validations/register.validate.js
@@ -3,31 +3,47 @@ const validator = require('validator/validator');
 module.exports = (requestBody) => {
   const { username, email, password, firstName, lastName } = requestBody;
 
-  let errorAsArray = [];
+  let errorAsObject = {};
 
-  if (username && email && password && firstName && lastName) {
+  if (username) {
     if (!validator.isSlug(username)) {
-      errorAsArray.push(`please provide valid username`);
-    }
-
-    if (!validator.isEmail(email)) {
-      errorAsArray.push(`please provide valid email`);
-    }
-
-    if (!validator.isAlphanumeric(firstName, 'en-US', { ignore: ' -' })) {
-      errorAsArray.push(`please provide valid first Name`);
-    }
-
-    if (!validator.isAlphanumeric(lastName, 'en-US', { ignore: ' -' })) {
-      errorAsArray.push(`please provide valid last Name`);
-    }
-
-    if (!validator.isStrongPassword(password)) {
-      errorAsArray.push(`please provide strong password`);
+      errorAsObject.username = `please provide valid username`;
     }
   } else {
-    errorAsArray.push(`make sure username or email and password are provided`);
+    errorAsObject.username = `username is missing`;
   }
 
-  return errorAsArray;
+  if (email) {
+    if (!validator.isEmail(email)) {
+      errorAsObject.email = `please provide valid email`;
+    }
+  } else {
+    errorAsObject.email = `email is missing`;
+  }
+
+  if (firstName) {
+    if (!validator.isAlphanumeric(firstName, 'en-US', { ignore: ' -' })) {
+      errorAsObject.firstName = `please provide valid first Name`;
+    }
+  } else {
+    errorAsObject.firstName = `firstName is missing`;
+  }
+
+  if (lastName) {
+    if (!validator.isAlphanumeric(lastName, 'en-US', { ignore: ' -' })) {
+      errorAsObject.lastName = `please provide valid first Name`;
+    }
+  } else {
+    errorAsObject.lastName = `lastName is missing`;
+  }
+
+  if (password) {
+    if (!validator.isStrongPassword(password)) {
+      errorAsObject.password = `please provide strong password`;
+    }
+  } else {
+    errorAsObject.password = `password is missing`;
+  }
+
+  return errorAsObject;
 };
