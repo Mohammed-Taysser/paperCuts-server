96bcd696a5887e9a34b2d77be7bc142b4d3266bf
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-25 13:21:23 +0200

    create cart controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,73 +1,77 @@
-const mongoose = require('mongoose');
 const schema = require('../models/cart.schema');
 const statusCode = require('../utilities/statusCode');
 const catchError = require('../utilities/catchError');
 
-exports.all = async (_, response) => {
+exports.all = async (request, response) => {
+  'use strict';
+  const { username } = request.author;
+
   await schema
-    .find()
+    .find({ username })
     .then((results) => {
       response.status(statusCode.success.ok).json(results);
     })
-    .catch((error) =>
-      catchError(response, statusCode.error.serverError, error.message)
-    );
+    .catch((error) => {
+      catchError(response, statusCode.error.serverError, error.message);
+    });
 };
 
 exports.create = async (request, response) => {
-  const { title, info, img } = request.body;
-  if (title && info && img) {
-    let createdService = new schema(request.body);
+  'use strict';
+  const createdItem = new schema({
+    ...request.body,
+    username: request.author.username,
+  });
 
-    await createdService
-      .save()
-      .then((results) => {
-        response.status(statusCode.success.created).json(results);
-      })
-      .catch((error) =>
-        catchError(response, statusCode.error.conflict, error.message)
-      );
-  } else {
-    catchError(response, statusCode.error.badRequest, 'coordinate are missing');
-  }
+  await createdItem
+    .save()
+    .then((results) => {
+      response.status(statusCode.success.created).json(results);
+    })
+    .catch((error) => {
+      catchError(response, statusCode.error.conflict, error.message);
+    });
 };
 
-exports.update = async (request, response) => {
-  const _id = request.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(_id)) {
-    catchError(
-      response,
-      statusCode.error.notFound,
-      `No Corresponding Serving With ${_id}`
-    ).end();
-  }
+exports.view = async (request, response) => {
+  'use strict';
+  const { bookId } = request.params,
+    { username } = request.author;
 
   await schema
-    .findByIdAndUpdate(_id, request.body, { new: true })
+    .findOne({ bookId, username })
     .then((results) => {
       response.status(statusCode.success.ok).json(results);
     })
-    .catch((error) =>
-      catchError(response, statusCode.error.serverError, error.message)
-    );
+    .catch((error) => {
+      catchError(response, statusCode.error.serverError, error.message);
+    });
+};
+
+exports.update = async (request, response) => {
+  'use strict';
+
+  await schema
+    .findByIdAndUpdate(
+      { _id: request.params.id },
+      { quantity: request.body.quantity },
+      { new: true },
+    )
+    .then((results) => {
+      response.status(statusCode.success.ok).json(results);
+    })
+    .catch((error) => {
+      catchError(response, statusCode.error.serverError, error.message);
+    });
 };
 
 exports.delete = async (request, response) => {
-  const _id = request.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(_id)) {
-    catchError(
-      response,
-      statusCode.error.notFound,
-      `No Corresponding Service With ${_id}`
-    ).end();
-  }
+  'use strict';
 
   await schema
-    .findByIdAndDelete(_id)
+    .findByIdAndDelete({ _id: request.params.id })
     .then((results) => {
-      response.status(statusCode.success.ok).send(results);
+      response.status(statusCode.success.ok).json(results);
     })
     .catch((error) => {
       catchError(response, statusCode.error.serverError, error.message);
