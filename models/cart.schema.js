8dce93cfcf8807265aded6d804e4995bc508a587
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-25 13:20:38 +0200

    create cart schema

--- models/cart.schema.js
+++ models/cart.schema.js
@@ -1,23 +1,25 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const mongoose = require('mongoose'),
+  { Schema } = mongoose,
+  cartSchema = new Schema({
+    username: {
+      type: String,
+      required: [ true, 'username not provided' ],
+    },
+    bookId: mongoose.Types.ObjectId,
+    slug: {
+      type: String,
+      required: [ true, 'username not provided' ],
+    },
+    quantity: {
+      type: Number,
+      required: [ true, 'Please specify user quantity' ],
+    },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: {
+      type: String,
+      required: [ true, 'image not provided' ],
+    },
+  });
 
-const cartSchema = new Schema({
-  userId: mongoose.Types.ObjectId,
-  slug: {
-    type: String,
-    required: [true, "username not provided"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "Please specify user quantity"],
-  },
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  type: { type: String, required: true },
-  avatar: {
-    type: String,
-    required: [true, "avatar not provided"],
-  },
-});
-
-module.exports = mongoose.model("Cart", cartSchema);
+module.exports = mongoose.model('Cart', cartSchema);
