1154d1d86642038b96bcb491797e1e9a1fa9bd93
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-25 13:20:18 +0200

    create cart route

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const controller = require("../controllers/cart.controller");
-const authToken = require("../middleware/auth-token");
+const express = require('express');
+const controller = require('../controllers/cart.controller');
+const authToken = require('../middleware/auth-token'),
+  router = express.Router();
 
-const router = express.Router();
-
-router.get("/", authToken, controller.all);
-router.post("/create", authToken, controller.create);
-router.put("/update/:id", authToken, controller.update);
-router.delete("/delete/:id", authToken, controller.delete);
+router.get('/', authToken, controller.all);
+router.get('/view/:bookId', authToken, controller.view);
+router.post('/create', authToken, controller.create);
+router.patch('/update/:id', authToken, controller.update);
+router.delete('/delete/:id', authToken, controller.delete);
 
 module.exports = router;
