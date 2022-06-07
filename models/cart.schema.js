65b00ac2f508785a500e65533bb541b38066d62b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-07 20:46:29 +0200

    lint with eslint

--- models/cart.schema.js
+++ models/cart.schema.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose'),
-  { Schema } = mongoose,
-  cartSchema = new Schema({
-    username: {
-      type: String,
-      required: [ true, 'username not provided' ],
-    },
-    bookId: mongoose.Types.ObjectId,
-    slug: {
-      type: String,
-      required: [ true, 'username not provided' ],
-    },
-    quantity: {
-      type: Number,
-      required: [ true, 'Please specify user quantity' ],
-    },
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: {
-      type: String,
-      required: [ true, 'image not provided' ],
-    },
-  });
+	{ Schema } = mongoose,
+	cartSchema = new Schema({
+		username: {
+			type: String,
+			required: [true, 'username not provided'],
+		},
+		bookId: mongoose.Types.ObjectId,
+		slug: {
+			type: String,
+			required: [true, 'slug not provided'],
+		},
+		quantity: {
+			type: Number,
+			required: [true, 'Please specify user quantity'],
+		},
+		title: { type: String, required: true },
+		price: { type: Number, required: true },
+		image: {
+			type: String,
+			required: [true, 'image not provided'],
+		},
+	});
 
 module.exports = mongoose.model('Cart', cartSchema);
