e1f13b577b4d01286e9c1d0f620e332ad686d9b8
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-07 20:48:02 +0200

    lint with eslint

--- utilities/jwt.js
+++ utilities/jwt.js
@@ -3,23 +3,25 @@ require('dotenv').config();
 
 const { JWT_SECRET_KEY, JWT_LIFE_TIME } = process.env;
 
-async function generateToken(author) {
-  return jwt.sign(
-    {
-      _id: author._id,
-      email: author.email,
-      username: author.username,
-      avatar: author.avatar,
-    },
-    JWT_SECRET_KEY,
-    {
-      expiresIn: JWT_LIFE_TIME,
-    }
-  );
+function generateToken(author) {
+	'use strict';
+	return jwt.sign(
+		{
+			_id: author._id,
+			email: author.email,
+			username: author.username,
+			avatar: author.avatar,
+		},
+		JWT_SECRET_KEY,
+		{
+			expiresIn: JWT_LIFE_TIME,
+		}
+	);
 }
 
-async function verifyToken(token, fn) {
-  return jwt.verify(token, JWT_SECRET_KEY, fn);
+function verifyToken(token, callback) {
+	'use strict';
+	return jwt.verify(token, JWT_SECRET_KEY, callback);
 }
 
 module.exports = { generateToken, verifyToken };
