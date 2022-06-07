ea4813b0ba7a04076c17d8cdc9c542f5fcf2051a
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-07 20:45:19 +0200

    add delete all author cart items route

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const controller = require('../controllers/cart.controller');
-const authToken = require('../middleware/auth-token'),
-  router = express.Router();
+const authToken = require('../middleware/auth-token');
+const router = express.Router();
 
 router.get('/', authToken, controller.all);
 router.get('/view/:bookId', authToken, controller.view);
 router.post('/create', authToken, controller.create);
 router.patch('/update/:id', authToken, controller.update);
+router.delete('/delete/all', authToken, controller.deleteAll);
 router.delete('/delete/:id', authToken, controller.delete);
 
 module.exports = router;
