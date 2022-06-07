aa7ebaafb6fa5888b4d9901944ecf038638fbe98
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-07 20:44:19 +0200

    add update password & avatar route

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const controller = require('../controllers/authors.controller');
-
-const router = express.Router();
+const controller = require('../controllers/authors.controller'),
+	authToken = require('../middleware/auth-token'),
+	multer = require('../utilities/multer'),
+	router = express.Router();
 
 router.get('/', controller.all);
+router.patch('/update', authToken, controller.update);
+router.post(
+	'/update-avatar',
+	authToken,
+	multer.single('avatar'),
+	controller.updateAvatar
+);
+router.post('/change-password', authToken, controller.changePassword);
+router.get('/search', controller.search);
+router.delete('/delete/:id', authToken, controller.delete);
 router.get('/:slug', controller.view);
 
 module.exports = router;
