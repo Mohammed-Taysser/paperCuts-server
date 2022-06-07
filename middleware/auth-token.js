bb94138b77fa1d0f3b1ff611f8e1e677ac61a58f
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-07 20:51:54 +0200

    lint with eslint

--- middleware/auth-token.js
+++ middleware/auth-token.js
@@ -3,25 +3,22 @@ const statusCode = require('../utilities/statusCode');
 const { verifyToken } = require('../utilities/jwt');
 
 module.exports = async (request, response, next) => {
-  const { authorization } = request.headers;
-  if (authorization) {
-   await verifyToken(authorization, (error, author) => {
-      if (error) {
-        catchError(
-          response,
-          statusCode.error.forbidden,
-          `you aren't authorize, token is invalid`
-        );
-      } else {
-        request.author = author;
-        next();
-      }
-    })
-  } else {
-    catchError(
-      response,
-      statusCode.error.unauthorized,
-      'no token provide'
-    );
-  }
+	'use strict';
+	const { authorization } = request.headers;
+	if (authorization) {
+		await verifyToken(authorization, (error, author) => {
+			if (error) {
+				catchError(
+					response,
+					statusCode.error.forbidden,
+					`you aren't authorize, token is invalid`,
+				);
+			} else {
+				request.author = author;
+				next();
+			}
+		});
+	} else {
+		catchError(response, statusCode.error.unauthorized, 'no token provide');
+	}
 };
