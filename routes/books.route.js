e7dc6ea5060701ce82a9626b8211d91b5a3d45ff
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-20 12:25:53 +0200

    add temp create book route

--- routes/books.route.js
+++ routes/books.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const controller = require('../controllers/books.controller');
+const authToken = require('../middleware/auth-token');
+const authRole = require('../middleware/auth-roles');
 
 const router = express.Router();
 
@@ -8,7 +10,7 @@ router.get('/top5', controller.top5);
 router.get('/latest', controller.latest);
 router.get('/related', controller.related);
 router.get('/search', controller.search);
+router.post('/create', authToken, authRole, controller.create);
 router.get('/:slug', controller.view);
-
 
 module.exports = router;
