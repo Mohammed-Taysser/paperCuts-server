2ccbc97ef64157a0d8528e39b7a7e72c40bce0b8
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-20 12:26:17 +0200

    update role middleware

--- middleware/auth-roles.js
+++ middleware/auth-roles.js
@@ -1,17 +1,27 @@
 const catchError = require('../utilities/catchError');
 const statusCode = require('../utilities/statusCode');
+const schema = require('../models/author.schema');
 
 const { AUTHORIZATION_ROLE } = process.env;
 
-module.exports = (request, response, next) => {
-  const { role } = request.headers;
-  if (role && AUTHORIZATION_ROLE.includes(role)) {
-    next();
-  } else {
-    catchError(
-      response,
-      statusCode.error.unauthorized,
-      "You Don't Have Permission"
-    );
-  }
+module.exports = async (request, response, next) => {
+	'use strict';
+
+	await schema
+		.findOne({ username: request.author.username })
+		.then((results) => {
+			const { role } = results;
+			if (role && AUTHORIZATION_ROLE.includes(role)) {
+				next();
+			} else {
+				catchError(
+					response,
+					statusCode.error.unauthorized,
+					"You Don't Have Permission"
+				);
+			}
+		})
+		.catch((error) => {
+			catchError(response, statusCode.error.unauthorized, error);
+		});
 };
