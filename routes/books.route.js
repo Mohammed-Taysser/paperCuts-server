61990f22ed8e3027ce33409a83fde8c39a3110c3
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-06 23:40:07 +0200

    create update & create book route

--- routes/books.route.js
+++ routes/books.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/books.controller');
 const authToken = require('../middleware/auth-token');
 const authRole = require('../middleware/auth-roles');
+const multer = require('../utilities/multer');
 
 const router = express.Router();
 
@@ -11,6 +12,13 @@ router.get('/latest', controller.latest);
 router.get('/related', controller.related);
 router.get('/search', controller.search);
 router.post('/create', authToken, authRole, controller.create);
+router.patch('/update', authToken, authRole, controller.update);
+router.post(
+	'/update-cover',
+	authToken,
+	multer.single('image'),
+	controller.updateCover
+);
 router.get('/:slug', controller.view);
 
 module.exports = router;
