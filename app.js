3901339aa270048cba6ae1e67ade6e93141c696f
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-07 14:33:12 +0300

    back to docs route

--- app.js
+++ app.js
@@ -56,10 +56,14 @@ app.use(
 );
 
 // Serving static files
-app.get('/static', express.static('static'));
+app.use('/static', express.static('static'));
 
 // Use / as docs route
-app.get('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/',(request,response)=>{
+	response.send('<h1>API <a href="/docs" >DOCS</a></h1>')
+})
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // connect to DB
 mongoose
